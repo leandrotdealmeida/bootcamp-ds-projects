85071d61c0e89b99420ebfb9bf3f58dd46091b1d
Leandro Almeida <leandrotrovilho@gmail.com>
2020-10-03 13:48:17 -0300

    Cõnfigurações iniciais do projeto

---
+++ dsclient/src/main/java/com/devjapa/DsclientApplication.java
@@ -0,0 +1,13 @@
+package com.devjapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientApplication.class, args);
+	}
+
+}
