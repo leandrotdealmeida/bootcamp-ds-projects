401788405932818bed70d16455dc3cd1aace7849
Leandro Almeida <leandrotrovilho@gmail.com>
2020-10-03 16:11:28 -0300

    Delete Client and Resource Exceptions

--- dsclient/src/main/java/com/devjapa/services/ClientService.java
+++ dsclient/src/main/java/com/devjapa/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devjapa.dto.ClientDTO;
 import com.devjapa.entities.Client;
 import com.devjapa.repositories.ClientRepository;
+import com.devjapa.services.exceptions.DatabaseException;
 import com.devjapa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,6 +64,17 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 
 }
