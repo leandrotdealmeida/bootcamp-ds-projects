5f836b128e00a946cf2d861d01dceea355adb9c9
Leandro Almeida <leandrotrovilho@gmail.com>
2020-10-03 15:06:43 -0300

    Find Client By Id

--- dsclient/src/main/java/com/devjapa/services/ClientService.java
+++ dsclient/src/main/java/com/devjapa/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.devjapa.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devjapa.dto.ClientDTO;
 import com.devjapa.entities.Client;
 import com.devjapa.repositories.ClientRepository;
+import com.devjapa.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -15,9 +19,17 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> listClients = repository.findAll(pageRequest);
 		return listClients.map(cli -> new ClientDTO(cli));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
+		return new ClientDTO(entity);
 	}
 	
 	
---
+++ dsclient/src/main/java/com/devjapa/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.devjapa.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {	
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String message) {
+		super(message);		
+	}	
+
+}
